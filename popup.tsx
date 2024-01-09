d017f97f83a97c5197abba2b2e2f303aa08424de
allen.lin <allen8673@gmail.com>
2024-01-09 14:56:28 +0800

    integrate eslint

--- popup.tsx
+++ popup.tsx
@@ -6,17 +6,10 @@ function IndexPopup() {
 
   return (
     <div className="p-2">
-      <h2 className="p-2">
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
+      <h2 className="p-2"   >
+        hello world
       </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+
     </div>
   )
 }
